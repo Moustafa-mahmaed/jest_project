d401e35fa68823c7c04435a77114dc93e7c1b9ac
moustafamahamedshamaa <moustafamahamedshamaa@gmail.com>
2020-03-16 17:44:14 +0200

    add header componet with style

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.scss'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="wrap">
+            <div className="logo">
+            <img src="https://www.freelogodesign.org/Content/img/logo-samples/flooop.png" alt="logo" />
+            </div>
+            
+            </div>
+       
+        </header>
+    )
+}
+
+export default Header
